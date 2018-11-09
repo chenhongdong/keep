22986e92432c868500578a651cedcb6b48c8b725
chenhongdong <513948670@qq.com>
2018-11-09 08:46:03 +0800

    'update'

---
+++ 弹幕/dm/index.js
@@ -0,0 +1,114 @@
+let $ = document.querySelector.bind(document);
+let canvas = $('#canvas');
+let video = $('#video');
+
+let canvasBarrage;
+let ws = new WebSocket('ws://localhost:2000');
+
+class Barrage {
+    constructor(obj, ctx) {
+        this.value = obj.value;
+        this.time = obj.time;
+        this.obj = obj;
+        this.context = ctx;
+    }
+    init() {
+        this.speed = this.obj.speed || this.context.speed;
+        this.color = this.obj.color || this.context.color;
+        this.fontSize = this.obj.fontSize ||　this.context.fontSize;
+        this.opacity = this.obj.opacity || this.context.opacity;
+
+        // 需要创建个span元素，来计算弹幕的宽
+        let span = document.createElement('span');
+        span.style.position = 'absolute';
+        span.style.font = `${this.fontSize}px`;
+        span.innerHTML = this.value;
+        document.body.appendChild(span);
+        // 获取每个弹幕的宽度，然后再把span删掉
+        this.width = span.clientWidth;
+        document.body.removeChild(span);
+
+        this.x = this.context.canvas.width;
+        this.y = this.context.canvas.height * Math.random();
+
+        if (this.y < this.fontSize) {
+            this.y = this.fontSize
+        } else if (this.y > this.context.canvas.height - this.fontSize) {
+            this.y = this.context.canvas.height - this.fontSize;
+        }
+    }
+    render() {
+        this.context.ctx.font = `${this.fontSize}px "Microsoft Yahei"`;
+        this.context.ctx.fillStyle = this.color;
+        this.context.ctx.fillText(this.value, this.x, this.y);
+    }
+}
+
+class CanvasBarrage {
+    constructor(canvas, video, options = {}) {
+        if (!canvas || !video) return;
+        this.canvas = canvas;
+        this.canvas.width = video.width;
+        this.canvas.height = video.height;
+        this.ctx = canvas.getContext('2d');
+        this.video = video;
+        // 默认参数
+        let defaultOpts = {
+            speed: 1.5,
+            color: '#00a1f4',
+            opacity: 0.4,
+            fontSize: 22,
+            data: []
+        };
+        Object.assign(this, defaultOpts, options);
+        // 是否暂停，默认true为暂停
+        this.isPaused = true;
+        // 所有的弹幕
+        this.barrages = this.data.map(obj => new Barrage(obj, this));
+        // 渲染弹幕
+        this.render();
+    }
+    render() {
+        this.clear();
+        this.renderBarrage();
+        if (this.isPaused === false) {
+            requestAnimationFrame(this.render.bind(this));
+        }
+    }
+    renderBarrage() {
+        let time = this.video.currentTime;
+        this.barrages.forEach(barrage => {
+            if (!barrage.flag && time >= barrage.time) {
+                if (!barrage.isInit) {
+                    barrage.init();
+                    barrage.isInit = true;
+                }
+                barrage.x -= barrage.speed;
+                barrage.render();
+                if (barrage.x < -barrage.width) {
+                    barrage.flag = true;
+                }
+            }
+        });
+    }
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+ws.onopen = function () {
+    ws.onmessage = function (e) {
+        let msg = JSON.parse(e.data);
+        console.log(msg);
+        if (msg.type === 'init') {
+            canvasBarrage = new CanvasBarrage(canvas, video, { data: msg.data });
+        } else if (msg.type === 'add') {
+            canvasBarrage.add(msg.data);
+        }
+    };
+};
+
+video.addEventListener('play', () => {
+    canvasBarrage.isPaused = false;
+    canvasBarrage.render();
+});
---
+++ 弹幕/dm/server.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+const redis = require('redis');
+const clientRedis = redis.createClient();
+const ws = new WebSocket.Server({ port: 2000 });
+
+let clients = [];
+
+ws.on('connection', socket => {
+    clients.push(socket);
+
+    clientRedis.lrange('barrages', 0, -1, (err, res) => {
+        res = res.map(data => JSON.parse(data));
+        socket.send(JSON.stringify({
+            type: 'init',
+            data: res
+        }));
+    });
+
+    socket.on('message', data => {
+        clientRedis.rpush('barrages', data, redis.print);
+        socket.send(JSON.stringify({
+            type: 'add',
+            data: JSON.parse(data)
+        }));
+    });
+
+    socket.on('close', () => {
+        clients = clients.filter(client => client !== socket);
+    });
+});
